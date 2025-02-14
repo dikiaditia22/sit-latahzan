b7d98e3eee2ffd2538aee33fe5cca8da5f316266
wafash08 <saefulhaqwafa@gmail.com>
2025-02-14 22:50:51 +0700

    menambahkan husky

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Hello guys</p>
-    </div>
-  );
-}
+	const pi = 3.14;
 
+	return (
+		<div>
+			<h1>Hello world</h1>
+			<p>Hello guys</p>
+		</div>
+	);
+}
