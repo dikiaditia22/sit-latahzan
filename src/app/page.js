ca198e88379aa1985b0a698a3140f464cd7b89eb
dikiaditia22 <aditiadiki102@gmail.com>
2025-02-15 22:53:53 +0700

    mengubah husky

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 export default function Home() {
-  const pi = 3.14;
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Hello guys</p>
-      <h2>Hello we</h2>
-      <br></br>
-    </div>
+    <section>
+      <h1 className="text-green-500 text-center">MOTTO <span className="text-black">LATAHZAN</span></h1>
+      <p className="text-center">"Bersenang-senang kemudian, bercerai-cerai menanti"</p>
+        <section className="flex justify-evenly">
+          <div className="border border-black-500">
+            <h2 className="text-center">VISI LATAHZAN</h2>
+            <p>Terwujudnya generasi yang Cerdas, Dedikasi, dan Amanah serta memiliki kemampuan sosial yang tinggi berdasarkan nilai-nilai Islam</p>
+          </div>
+          <div className="border border-black-500">
+            <h2 className="text-center">MISI LATAHZAN</h2>
+            <p>Mengenalkan ajaran Islam yang benar dan membiasakan berakhlak Islami sedini mungkin.
+            Membina anak didik menjadi cerdas, kreatif</p>
+          </div>
+          <div className="border border-black-500">
+            <h2 className="text-center">TUJUAN SEKOLAH</h2>
+            <p>Siswa dapat memahami dan menjalankan nilai-nilai Islam secara benar
+            Siswa memiliki dasar-dasar pengetahuan dan</p>
+          </div>
+        </section>
+    </section>
   );
 }
