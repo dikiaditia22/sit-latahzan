0a320994bf55e8d3d4b44148dc24d8e9f98d0cf9
dikiaditia22 <aditiadiki102@gmail.com>
2025-02-19 20:58:06 +0700

    menambahkandiv

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,35 @@
 export default function Home() {
 	return (
-		<section>
-			<h1 className="text-green-500 text-center">
+		<section className="w-full">
+			<h1 className="text-green-500 text-center font-bold text-3xl mt-6 ">
 				MOTTO <span className="text-black">LATAHZAN</span>
 			</h1>
-			<p className="text-center">
-				"Bersenang-senang kemudian, bercerai-cerai menanti"
+			<p className="text-center text-black-700 text-2xl mt-10 max-w-full">
+				"Smart And Religious"
 			</p>
-			<section className="flex justify-evenly">
-				<div className="border border-black-500">
-					<h2 className="text-center">VISI LATAHZAN</h2>
-					<p>
-						Terwujudnya generasi yang Cerdas, Dedikasi, dan Amanah serta
-						memiliki kemampuan sosial yang tinggi berdasarkan nilai-nilai Islam
+			<section className="flex justify-evenly mt-10 pt-10">
+				<div className="border border-black-500 w-96">
+					<h2 className="text-green-500 text-center font-bold mt-10">VISI <span className="text-black">LATAHZAN</span></h2>
+					<p className=" indent-5 mt-7 text-justify p-6">
+						Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
 					</p>
 				</div>
-				<div className="border border-black-500">
-					<h2 className="text-center">MISI LATAHZAN</h2>
-					<p>
-						Mengenalkan ajaran Islam yang benar dan membiasakan berakhlak Islami
-						sedini mungkin. Membina anak didik menjadi cerdas, kreatif
+				<div className="border border-black-500 w-96 ">
+					<h2 className="text-green-500 text-center font-bold mt-10">MISI <span className="text-black">LATAHZAN</span></h2>
+					<p className="mt-7 text-justify p-6">
+						- Menyelanggarakan pendidikan berdasarkan keislaman.
 					</p>
-				</div>
-				<div className="border border-black-500">
-					<h2 className="text-center">TUJUAN SEKOLAH</h2>
-					<p>
-						Siswa dapat memahami dan menjalankan nilai-nilai Islam secara benar
-						Siswa memiliki dasar-dasar pengetahuan dan
+					<p className="mt-1 text-justify p-6">
+						- Menanamkan keislaman berdasarkan al-Quran dan Sunnah.
+					</p>
+					<p className="mt-1 text-justify p-6">
+						- Melaksanakan program pendidikan, yang mendorong pola pikir maju dan kreatif.
+					</p>
+					<p className="mt-1 text-justify p-6">
+						- Memfasilitasi siswa dalam mengembangkan kemampuan dan pengetahuan.
+					</p>
+					<p className="mt-1 text-justify p-6">
+						- Meningkatkan prestasi, keterampilan dan kemandirian siswa.
 					</p>
 				</div>
 			</section>
