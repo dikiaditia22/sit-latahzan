a7394cab7ff2a316adf82447942850cbb8e6ff44
dikiaditia22 <aditiadiki102@gmail.com>
2025-02-19 20:59:31 +0700

    menambahkandiv

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,18 @@ export default function Home() {
 			</p>
 			<section className="flex justify-evenly mt-10 pt-10">
 				<div className="border border-black-500 w-96">
-					<h2 className="text-green-500 text-center font-bold mt-10">VISI <span className="text-black">LATAHZAN</span></h2>
+					<h2 className="text-green-500 text-center font-bold mt-10">
+						VISI <span className="text-black">LATAHZAN</span>
+					</h2>
 					<p className=" indent-5 mt-7 text-justify p-6">
-						Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
+						Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu
+						pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
 					</p>
 				</div>
 				<div className="border border-black-500 w-96 ">
-					<h2 className="text-green-500 text-center font-bold mt-10">MISI <span className="text-black">LATAHZAN</span></h2>
+					<h2 className="text-green-500 text-center font-bold mt-10">
+						MISI <span className="text-black">LATAHZAN</span>
+					</h2>
 					<p className="mt-7 text-justify p-6">
 						- Menyelanggarakan pendidikan berdasarkan keislaman.
 					</p>
@@ -23,7 +28,8 @@ export default function Home() {
 						- Menanamkan keislaman berdasarkan al-Quran dan Sunnah.
 					</p>
 					<p className="mt-1 text-justify p-6">
-						- Melaksanakan program pendidikan, yang mendorong pola pikir maju dan kreatif.
+						- Melaksanakan program pendidikan, yang mendorong pola pikir maju
+						dan kreatif.
 					</p>
 					<p className="mt-1 text-justify p-6">
 						- Memfasilitasi siswa dalam mengembangkan kemampuan dan pengetahuan.
