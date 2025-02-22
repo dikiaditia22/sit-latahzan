1bc5a39b3ad88f0048a9a7a3544d70b6f8ceedc2
dikiaditia22 <aditiadiki102@gmail.com>
2025-02-22 21:09:22 +0700

    menambahkan section

--- src/app/page.js
+++ src/app/page.js
@@ -1,44 +1,80 @@
 export default function Home() {
 	return (
-		<section className="w-full">
-			<h1 className="text-green-500 text-center font-bold text-3xl mt-6 ">
-				MOTTO <span className="text-black">LATAHZAN</span>
-			</h1>
-			<p className="text-center text-black-700 text-2xl mt-10 max-w-full">
-				"Smart And Religious"
-			</p>
-			<section className="flex justify-evenly mt-10 pt-10">
-				<div className="border border-black-500 w-96">
-					<h2 className="text-green-500 text-center font-bold mt-10">
-						VISI <span className="text-black">LATAHZAN</span>
-					</h2>
-					<p className=" indent-5 mt-7 text-justify p-6">
-						Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu
-						pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
-					</p>
+		<>
+			<section className=" bg-yellow-400 mx-auto">
+				<div className="flex justify-between items-center container mx-auto pt-3">
+					<img className="w-24" src="/jsit.png" alt="" />
+					<h1 className="text-center font-bold text-2xl">SEJARAH LATAHZAN</h1>
+					<img className="w-24" src="sditlatahzan.png" alt="" />
 				</div>
-				<div className="border border-black-500 w-96 ">
-					<h2 className="text-green-500 text-center font-bold mt-10">
-						MISI <span className="text-black">LATAHZAN</span>
-					</h2>
-					<p className="mt-7 text-justify p-6">
-						- Menyelanggarakan pendidikan berdasarkan keislaman.
-					</p>
-					<p className="mt-1 text-justify p-6">
-						- Menanamkan keislaman berdasarkan al-Quran dan Sunnah.
-					</p>
-					<p className="mt-1 text-justify p-6">
-						- Melaksanakan program pendidikan, yang mendorong pola pikir maju
-						dan kreatif.
-					</p>
-					<p className="mt-1 text-justify p-6">
-						- Memfasilitasi siswa dalam mengembangkan kemampuan dan pengetahuan.
-					</p>
-					<p className="mt-1 text-justify p-6">
-						- Meningkatkan prestasi, keterampilan dan kemandirian siswa.
+			</section>
+			<section className=" bg-green-600 mx-auto">
+				<h1 className="text-center font-bold text-4xl pt-6 ">
+					MOTTO <span className="text-yellow-400">LATAHZAN</span>
+				</h1>
+				<p className="text-center text-white text-2xl mt-10 max-w-full italic">
+					"Smart And Religious"
+				</p>
+				<section className="justify-wrap mt-10 mx-auto container ">
+					<div className=" w-full rounded-e-xl border-solid mb-8">
+						<h2 className=" text-justify font-bold mt-10 pl-4 text-3xl">
+							VISI <span className="text-yellow-400">LATAHZAN</span>
+						</h2>
+						<p className=" text-justify mt-3 p-4">
+							Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu
+							pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
+						</p>
+					</div>
+					<div className=" w-full rounded-s-xl border-solid pt-10 mb-8">
+						<h2 className="text-justify font-bold mt-10 pl-4 text-3xl">
+							MISI <span className="text-yellow-400">LATAHZAN</span>
+						</h2>
+						<p className="mt-3 text-justify p-4">
+							1. Menyelanggarakan pendidikan berdasarkan keislaman.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							2. Menanamkan keislaman berdasarkan al-Quran dan Sunnah.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							3. Melaksanakan program pendidikan, yang mendorong pola pikir maju
+							dan kreatif.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							4. Memfasilitasi siswa dalam mengembangkan kemampuan dan
+							pengetahuan.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							5. Meningkatkan prestasi, keterampilan dan kemandirian siswa.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							6. Mencintai dan menjadikan budaya Nasional sebagai Identitas
+							diri.
+						</p>
+						<p className="mt-3 text-justify p-4">
+							7. Mewujudkan lingkungan sekolah bersih, sehat dan nyaman.
+						</p>
+					</div>
+					<div className=" w-full rounded-e-xl border-solid mb-8">
+						<h2 className="text-justify font-bold mt-10 pl-4 text-3xl">
+							TUJUAN <span className="text-yellow-400">LATAHZAN</span>
+						</h2>
+						<p className=" text-justify mt-3 p-4">
+							Membentuk generasi Rabbani yang menguasai serta memanfaatkan ilmu
+							pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
+						</p>
+					</div>
+				</section>
+			</section>
+			<section>
+				<div className="mx-auto w-full max-w-xl">
+					<h1 className="text-center">PENGURUS DAN PENGAJAR</h1>
+					<p className="text-justify">
+						Untuk dapat membina pendidikan yang baik, Yayasan menyediakan
+						tenaga-tenaga yang kapabel dan berpengalaman. Dimana seluruh tenaga
+						pendidik berijazah S1 dan beberapa ada yang berijazah S2.
 					</p>
 				</div>
 			</section>
-		</section>
+		</>
 	);
 }
