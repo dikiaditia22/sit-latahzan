4e708f8369c0802cd63a270e5d8702e53a8d9382
dikiaditia22 <aditiadiki102@gmail.com>
2025-03-08 22:41:11 +0700

    membuat section

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,35 @@ export default function Home() {
 				<div className="flex justify-between items-center container mx-auto p-7">
 					<img className="w-24" src="/jsit.png" alt="" />
 					<h1 className="text-center font-bold text-4xl">SEJARAH LATAHZAN</h1>
-					<img className="w-24" src="sditlatahzan.png" alt=""/>
+					<img className="w-24" src="sditlatahzan.png" alt="" />
 				</div>
 				<div className="mx-auto container p-10">
-					<p className="text-justify text-xl tracking-wide pb-2 indent-10">Sejak berdirinya Yayasan Assalam unit pertama yang bergerak adalah Pesantren Yatim Assalam dengan tujuan non provit Alhamdulillah banyakn Alumni Santri Pesantren Assalam sejak berdirinya pada tahun 1990 yang sudah mendapatkan manfaatnya sejak saat itu berdirilah lembaga pendidikan RA Assalam pada tahun 2002 kemudian unit baru SDIT Latahzan pada tahun 2011 dengan jumlah 10 Siswa dan Alhamdulillah sejak saat ini mendirikan Rumah Quran Latahzan ditahun 2024 dan menyusul pada tahun ini 2025 berdiri SMPIT Latahzan.</p>
-					<p className="text-justify text-xl tracking-wide pb-2 indent-10">SDIT Latahzan berdiri sejak 2011. Dengan niat dakwah dan semangat untuk mendirikan Lembaga Pendidikan Islam Terpadu yang memplopori diwilayah khususnya Kecamatan Mauk dengan jumlah peserta didik 10 Siswa, beriringnya waktu Alhamdulillah sejak saat berdiri sudah banyak prestasi diberbagai bidang, Para Alumni tersebar dipesantren Favorit dan juga Sekolah Negeri Favorit sekitar Tangerang. </p>
-					<p className="text-justify text-xl tracking-wide pb-2 indent-10">Berkat tekad yang kuat kami demi meningkatkan mutu dan kualitas pendidikan, SDIT Latahzan membuka kelas Excellent berbasis bilingual dengan sistem fullday school, Alhamdulillah Antusiasme dan kepercayaan masyarakat makin meningkat untuk mengamanahkan putra putrinya belajar di SDIT Latahzan. Semoga lahir lah generasi robbani yang paham agama dan memadukan keilmuannya untuk kepentingan umat.</p>
+					<p className="text-justify text-xl tracking-wide pb-2 indent-10">
+						Sejak berdirinya Yayasan Assalam unit pertama yang bergerak adalah
+						Pesantren Yatim Assalam dengan tujuan non provit Alhamdulillah
+						banyakn Alumni Santri Pesantren Assalam sejak berdirinya pada tahun
+						1990 yang sudah mendapatkan manfaatnya sejak saat itu berdirilah
+						lembaga pendidikan RA Assalam pada tahun 2002 kemudian unit baru
+						SDIT Latahzan pada tahun 2011 dengan jumlah 10 Siswa dan
+						Alhamdulillah sejak saat ini mendirikan Rumah Quran Latahzan ditahun
+						2024 dan menyusul pada tahun ini 2025 berdiri SMPIT Latahzan.
+					</p>
+					<p className="text-justify text-xl tracking-wide pb-2 indent-10">
+						SDIT Latahzan berdiri sejak 2011. Dengan niat dakwah dan semangat
+						untuk mendirikan Lembaga Pendidikan Islam Terpadu yang memplopori
+						diwilayah khususnya Kecamatan Mauk dengan jumlah peserta didik 10
+						Siswa, beriringnya waktu Alhamdulillah sejak saat berdiri sudah
+						banyak prestasi diberbagai bidang, Para Alumni tersebar dipesantren
+						Favorit dan juga Sekolah Negeri Favorit sekitar Tangerang.{" "}
+					</p>
+					<p className="text-justify text-xl tracking-wide pb-2 indent-10">
+						Berkat tekad yang kuat kami demi meningkatkan mutu dan kualitas
+						pendidikan, SDIT Latahzan membuka kelas Excellent berbasis bilingual
+						dengan sistem fullday school, Alhamdulillah Antusiasme dan
+						kepercayaan masyarakat makin meningkat untuk mengamanahkan putra
+						putrinya belajar di SDIT Latahzan. Semoga lahir lah generasi robbani
+						yang paham agama dan memadukan keilmuannya untuk kepentingan umat.
+					</p>
 				</div>
 			</section>
 			<section className=" bg-white mx-auto">
@@ -64,19 +87,25 @@ export default function Home() {
 							TUJUAN <span className="text-green-700">LATAHZAN</span>
 						</h2>
 						<p className="text-justify text-xl mt-3 p-4">
-							Membentuk generasi Rabbani yang taat pada Allah SWT dan Rasulnya, memahami nilai-nilai Islam dan mencetak calon para penghafal Al-Quran serta menguasai & memanfaatkan ilmu
-							pengetahuan dan teknologi dengan pola pikir maju dan kreatif.
+							Membentuk generasi Rabbani yang taat pada Allah SWT dan Rasulnya,
+							memahami nilai-nilai Islam dan mencetak calon para penghafal
+							Al-Quran serta menguasai & memanfaatkan ilmu pengetahuan dan
+							teknologi dengan pola pikir maju dan kreatif.
 						</p>
 					</div>
 				</section>
 			</section>
 			<section className="mx-auto bg-blue-700">
 				<div className="mx-auto w-3/4 container">
-					<h1 className="text-center font-bold text-4xl pt-6 pb-10 text-yellow-400">PENGURUS <span className="text-white">DAN</span> <span className="text-yellow-400">PENGAJAR</span> </h1>
+					<h1 className="text-center font-bold text-4xl pt-6 pb-10 text-yellow-400">
+						PENGURUS <span className="text-white">DAN</span>{" "}
+						<span className="text-yellow-400">PENGAJAR</span>{" "}
+					</h1>
 					<p className="text-justify text-2xl text-white">
-						Untuk dapat membina pendidikan yang baik, Yayasan As-Salam menyediakan
-						tenaga-tenaga yang kapabel dan berpengalaman. Dimana seluruh tenaga
-						pendidik berijazah S1 dan beberapa ada yang berijazah S2.
+						Untuk dapat membina pendidikan yang baik, Yayasan As-Salam
+						menyediakan tenaga-tenaga yang kapabel dan berpengalaman. Dimana
+						seluruh tenaga pendidik berijazah S1 dan beberapa ada yang berijazah
+						S2.
 					</p>
 				</div>
 			</section>
